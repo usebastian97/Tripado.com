e2b6273a9ea7cdba61dbddd4a806270ea3ea9a8c
Sebastian Utoiu <sebiutoiu12@gmail.com>
2021-01-24 12:57:26 +0200

    Initial commit

---
+++ app/src/main/java/com/usebastian/tripadoo/MainActivity.java
@@ -0,0 +1,65 @@
+package com.usebastian.tripadoo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+{
+
+	private DrawerLayout drawerLayout;
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_main );
+
+		Toolbar toolbar = findViewById( R.id.toolbar );
+
+		drawerLayout = findViewById( R.id.drawer_layout );
+
+		NavigationView navigationView = findViewById( R.id.nav_view );
+		navigationView.setNavigationItemSelectedListener( this );
+
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this, drawerLayout, toolbar,
+				R.string.navigation_drawer_open, R.string.navigation_drawer_close );
+		drawerLayout.addDrawerListener( toggle );
+		toggle.syncState();
+	}
+
+	@Override
+	public boolean onNavigationItemSelected( @NonNull MenuItem item )
+	{
+
+		if( item.getItemId() == R.id.nav_romania )
+		{
+			startActivity( new Intent( this, RomaniaActivity.class ) );
+		}
+
+		drawerLayout.closeDrawer( GravityCompat.START );
+		return true;
+	}
+
+	@Override
+	public void onBackPressed()
+	{
+		if( drawerLayout.isDrawerOpen( GravityCompat.START ) )
+		{
+			drawerLayout.closeDrawer( GravityCompat.START );
+		}
+		else
+		{
+			super.onBackPressed();
+		}
+	}
+}
---
+++ app/src/main/java/com/usebastian/tripadoo/RomaniaActivity.java
@@ -0,0 +1,16 @@
+package com.usebastian.tripadoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RomaniaActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_romania );
+	}
+}
